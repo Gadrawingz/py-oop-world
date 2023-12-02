978b8fed93a148b7b1ef10a8b785670127e7c33b
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2023-12-02 23:27:47 +0200

    objects uploaded

---
+++ objects/car_object.py
@@ -0,0 +1,13 @@
+# defining class
+class Car:
+    name = "Mahindra"
+    tyre = 2
+    seats = 9
+
+# create objects of class
+# car1 is the object of the class.
+# It can be used to access the class attributes.
+car1 = Car()
+print(car1.name)  # Mahindra
+print(car1.tyre)  # Mahindra
+print(car1.seats)  # Mahindra
